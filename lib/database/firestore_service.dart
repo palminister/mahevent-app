683d48a71c9ab31d851c327b720403662459863f
SlothPete7773 <Veerakit413@gmail.com>
2022-04-22 02:06:32 +0700

    Added firestorage service

--- lib/database/firestore_service.dart
+++ lib/database/firestore_service.dart
@@ -1,5 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:mahevent/model/event.dart';
+import 'dart:io';
+
+// import 'package:image_picker/image_picker.dart';
 
 class DatabaseService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -22,7 +27,7 @@ class DatabaseService {
         'https://firebasestorage.googleapis.com/v0/b/flutter-mahevent.appspot.com/o/event_images%2Fmusic_concert.jpeg?alt=media&token=98f2aeea-8c19-4bf2-af89-f70b6a9aa60e';
     const hostImageTemp = 'assets/guest_images/pete1.jpg';
     return _events.add({
-      'imagePath': imageTemp,
+      'imagePath': event.imagePath,
       'eventId': 5,
       'title': event.title,
       'description': event.description,
@@ -31,7 +36,7 @@ class DatabaseService {
       'duration': event.duration,
       'h1': event.h1,
       'h2': event.h2,
-      'host': event.host,
+      'host': 'mock_host',
       'hostImage': hostImageTemp,
       'categoryIds': event.categoryIds,
       'eventImages': [imageTemp]
@@ -51,5 +56,20 @@ class DatabaseService {
       'categoryIds': [0, 1, 2, 3],
       'eventImages': [imageTemp]
     });
+  }
+
+  Future<String> uploadImage(XFile source) async {
+    // Reference ref =
+    //     FirebaseStorage.instance.ref().child('event-images').child('');
+
+    // UploadTask _uploadTask;
+    // _uploadTask = ref.putData(await _imageFile!.readAsBytes());
+
+    TaskSnapshot _snapshot = await FirebaseStorage.instance
+        .ref()
+        .child('event-images')
+        .child('${DateTime.now()}-${source.name}')
+        .putFile(File(source.path));
+    return _snapshot.ref.getDownloadURL();
   }
 }
