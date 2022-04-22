50452936a5366432983773e5ff6e455447c50823
SlothPete7773 <Veerakit413@gmail.com>
2022-04-22 22:43:30 +0700

    Update user info

--- lib/ui/profile/editProfile.dart
+++ lib/ui/profile/editProfile.dart
@@ -15,10 +15,21 @@ class EditProfile extends StatefulWidget {
 
 class _EditProfileState extends State<EditProfile> {
   final DatabaseService _service = DatabaseService();
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    // _initRetrieval();
+  }
+
+  // Future<void> _initRetrieval() async {
+  //   _user = _service.getUser();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    User user = mainUser;
+    // User user = mainUser;
     final formKey = GlobalKey<FormState>();
 
     return Scaffold(
@@ -27,104 +38,113 @@ class _EditProfileState extends State<EditProfile> {
         child: Form(
           key: formKey,
           child: (SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                ProfileWidget(
-                  imagePath: user.imagePath,
-                  isEdit: true,
-                  onClicked: () async {},
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Name",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      TextFormField(
-                        initialValue: user.name,
-                        onSaved: (name) {
-                          user.name = name!;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        "Username",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      TextFormField(
-                        initialValue: user.username,
-                        onSaved: (username) {
-                          user.username = username!;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        "About",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      TextFormField(
-                        initialValue: user.about,
-                        maxLines: 3,
-                        onSaved: (about) {
-                          user.about = about!;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        "Interest",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      TextFormField(
-                        initialValue: user.interest,
-                        maxLines: 2,
-                        onSaved: (interest) {
-                          user.interest = interest!;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      SizedBox(
-                          width: 400,
-                          height: 50,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.pink,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(12)),
-                            ),
-                            onPressed: () async {
-                              formKey.currentState?.save();
-                              print(user.name);
-                              print(user.interest);
-                              print(user.username);
-                              print(user.about);
-                              await _service.editUser(user);
-                              formKey.currentState?.reset();
-                            },
-                            child: Text('Save',
-                                style:
-                                    h2TextStyle.copyWith(color: Colors.white)),
-                          ))
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )),
+              child: FutureBuilder(
+            future: _service.getUser(),
+            builder: (context, AsyncSnapshot<User> snapshot) {
+              if (snapshot.hasData) {
+                user = snapshot.data!;
+                print(user!.name);
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ProfileWidget(
+                      imagePath: user!.imagePath,
+                      isEdit: true,
+                      onClicked: () async {},
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Name",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          initialValue: user!.name,
+                          onSaved: (name) {
+                            user!.name = name!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          "Username",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          initialValue: user!.username,
+                          onSaved: (username) {
+                            user!.username = username!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          "About",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          initialValue: user!.about,
+                          maxLines: 3,
+                          onSaved: (about) {
+                            user!.about = about!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          "Interest",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextFormField(
+                          initialValue: user!.interest,
+                          maxLines: 2,
+                          onSaved: (interest) {
+                            user!.interest = interest!;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        SizedBox(
+                            width: 400,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.pink,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12)),
+                              ),
+                              onPressed: () async {
+                                formKey.currentState?.save();
+                                print(user!.name);
+                                print(user!.interest);
+                                print(user!.username);
+                                print(user!.about);
+                                await _service.editUser(user!);
+                                // formKey.currentState?.reset();
+                              },
+                              child: Text('Save',
+                                  style: h2TextStyle.copyWith(
+                                      color: Colors.white)),
+                            ))
+                      ],
+                    )
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ))),
         ),
       ),
     );
